066a28147363e8a5bfc7fd92b6c892cd4f2919dc
fataranto <fataranto@gmail.com>
2022-08-05 16:46:25 +0200

    ex 1845 1 ok

--- UF1845/exercici1/app.js
+++ UF1845/exercici1/app.js
@@ -3,3 +3,14 @@
 //     "matricula": "F 1234 TL",
 //     "diesel": true
 // };
+
+// cargar el fichero cotxe_benzina.json
+
+const fs = require('fs');
+const cotxe = JSON.parse(fs.readFileSync('cotxe_benzina.json', 'utf8'));
+
+let gas = cotxe.diesel ? 'diesel' : 'gasolina';
+
+let response = `El model del cotxe es ${cotxe.model}, la seva matrícula és ${cotxe.matricula}, i funciona amb ${gas}`;
+
+console.log(response);
